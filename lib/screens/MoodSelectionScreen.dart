edf905c73ed472481f0e911fbf0fffd8090fd125
eli409 <diana.elisa.tita@gmail.com>
2025-01-03 14:01:32 +0100

    [+] added home screen design

--- lib/screens/MoodSelectionScreen.dart
+++ lib/screens/MoodSelectionScreen.dart
@@ -17,52 +17,70 @@ class _MoodSelectionScreenState extends State<MoodSelectionScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueAccent[100],
-      body: Column(
-        children: [
-          Container(
-            height: 157,
-            decoration: const BoxDecoration(
-              color: Color.fromARGB(255, 0, 24, 67),
-            ),
-            child: const Center(
-              child: Text(
-                "How are you feeling?",
-                style: TextStyle(
-                  fontSize: 26,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 158),
-          DropdownButton<String>(
-            value: selectedMood,
-            hint: const Text("Select your mood"),
-            items: const [
-              DropdownMenuItem(value: "Happy", child: Text("Happy")),
-              DropdownMenuItem(value: "Sad", child: Text("Sad")),
-              DropdownMenuItem(value: "Hopeful", child: Text("Hopeful")),
-              DropdownMenuItem(value: "Angry", child: Text("Angry")),
-            ],
-            onChanged: (value) {
-              setState(() {
-                selectedMood = value;
-              });
-
-              if (value != null) {
-                // Navigate to the playlist screen with the selected mood
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PlaylistScreen(mood: value),
+      body: 
+        Container(
+            decoration: 
+              const BoxDecoration(
+                gradient:  
+                  LinearGradient(
+                    colors: [
+                      Color(0xFFB0E0E6),
+                      Color(0xFF4682B4),
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter, 
                   ),
-                );
-              }
-            },
-          ),
-        ],
-      ),
+              ),
+            child: Column(
+              children: [
+                Container(
+                  height: 157,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF4682B4),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(25), 
+                      bottomRight: Radius.circular(25),
+                    ),              
+                  )
+                  child: const Center(
+                    child: Text(
+                      "How are you feeling?",
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 158),
+                DropdownButton<String>(
+                  value: selectedMood,
+                  hint: const Text("Select your mood"),
+                  items: const [
+                    DropdownMenuItem(value: "Happy", child: Text("Happy")),
+                    DropdownMenuItem(value: "Sad", child: Text("Sad")),
+                    DropdownMenuItem(value: "Hopeful", child: Text("Hopeful")),
+                    DropdownMenuItem(value: "Angry", child: Text("Angry")),
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      selectedMood = value;
+                    });
+
+                    if (value != null) {
+                    // Navigate to the playlist screen with the selected mood
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PlaylistScreen(mood: value),
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ],
+            ),
+        ),
     );
   }
 }
